326d543c160b21610285f8a71a0080de5ed3cee0
Sam Spilsbury <smspillaz@gmail.com>
2015-07-28 11:19:42 +0800

    Install most recent version

--- travisbumpversion/main.py
+++ travisbumpversion/main.py
@@ -92,7 +92,7 @@ def _get_level():
 
 def _last_tag_from_git():
     """Read known tag from git."""
-    subprocess.check_call(["git", "fetch", "--tags"])
+    subprocess.check_call(["git", "fetch", "--tags"])  # pragma: no cover
     return subprocess.check_output(["git",  # pragma: no cover
                                     "describe",
                                     "--tags"]).decode("utf-8").strip()
