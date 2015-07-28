c180d9188c4b2345104226620b4e5542ad35d6bf
Travis-CI <travis@travis-ci.org>
2015-07-28 04:18:54 +0000

    Bump version: 0.0.17 → 0.0.18

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name="travis-bump-version",
-      version="0.0.17",
+      version="0.0.18",
       description="Bump version files on travis builds",
       long_description_markdown_filename="README.md",
       author="Sam Spilsbury",
